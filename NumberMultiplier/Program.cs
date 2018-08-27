a2558a0b0d8546f2c8a9f3862929824d2de6e06b
Leo Taylor <leotaylor723@gmail.com>
2018-08-27 17:48:31 -0500

    multiplies

--- NumberMultiplier/Program.cs
+++ NumberMultiplier/Program.cs
@@ -6,7 +6,39 @@ namespace NumberMultiplier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Numbers separated by a comma.");
+            var numbers = Console.ReadLine();
+            var numbersArray = numbers.Split(',');
+            Console.WriteLine("Multiply 'm' or Square 's'?");
+            var mOrS = Console.ReadLine();
+            if (mOrS == "m")
+            {
+                int answer = 0;
+                for(int i = 0; i < numbersArray.Length; i++)
+                {
+                    int currentNumber = int.Parse(numbersArray[i]);
+                    if(i == 0)
+                    {
+                        answer = currentNumber;
+                    }
+                    else
+                    {
+                        answer = answer * currentNumber;
+                    }
+                }
+                Console.WriteLine(answer);
+                Console.ReadLine();
+            } else
+            {
+                if (mOrS == "s")
+                {
+                    int answer = 0;
+                    for(int i = 0; i < numbersArray.Length; i++)
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
